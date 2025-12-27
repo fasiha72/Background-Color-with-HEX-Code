f2f7f60885412129a891890671765008b5d5cd48
Fasiha <fasihaashraf72@gmail.com>
2025-12-27 13:08:48 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+const button = document.getElementById('colorBtn');
+const colorCode = document.getElementById('colorCode');
+
+button.addEventListener('click', () => {
+    const randomColor = getRandomColor();
+    document.body.style.backgroundColor = randomColor;
+    colorCode.textContent = randomColor;
+});
+
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
